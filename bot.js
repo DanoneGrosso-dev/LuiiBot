e2c5662e733c946f0c28bf21a60de04582236f9c
Gustavoluii <36324212+Gustavoluii@users.noreply.github.com>
2018-06-07 18:29:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,12 +67,28 @@ bot.on('ready', () => {
     bot.user.setActivity('www.mineluii.com | !loja', {type: 'PLAYING'});
 }); 
 
-bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('452304554977918976').send("Bem-Vindo " + member.user + " ao 🏆 Luii");
-
-});
+//bot.on('guildMemberAdd', member => {
+//    member.guild.channels.get('452304554977918976').send("Bem-Vindo " + member.user + " ao 🏆 Luii");
+//
+//});
 
 bot.on('guildMemberAdd', member => {
     bot.guilds.get(member.guild.id).members.get(member.id).sendMessage(`Bem-Vindo ${member} ao ${member.guild.name}\n \n:star2: MINECRAFT ORIGINAL E BARATO :star2:\n \nConta FULL ACESSO TOTAL!!!\n- Acesso para alterar o email\n- Acesso para alterar perguntas de segurança\n- Acesso para alterar senha\n- Acesso para alterar nick\n- Acesso para alterar a skin\n \n Loja Confiável de minha autoria. | GustavoLuii ©\nGrupo do Discord: https://discord.gg/BcnX29f\nSite: https://mineluii.com/\n \nDeseja comprar uma? chame o @!GustavoLuii#9510 em seu privado ou acesse nosso site!`);
 
 })
+
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '🔮novos-membros');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('8A0886')
+        .addField('Bem-Vindo!', `Bem-Vindo ${member} ao ${member.guild.name}! agora nós temos **${member.guild.memberCount}** Players em nosso servidor`)
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "has joined" + `${member.guild.name}`)
+
+});
